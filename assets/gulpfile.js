d7972200cccf5a81e3e8b988615f4272e3df7fb9
timothée <temurimi@gmail.com>
2023-09-30 08:50:02 +0200

    sitemap-images.xml

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -1,20 +1,19 @@
-const gulp = require('gulp');
-const cleanCSS = require('gulp-clean-css');
-const uglify = require('gulp-uglify');
+const gulp = require("gulp");
+const cleanCSS = require("gulp-clean-css");
+const uglify = require("gulp-uglify");
 
 // Fonction pour minifier les fichiers CSS
 function minifyCSS() {
-  return gulp.src('*.css')
+  return gulp
+    .src("*.css")
     .pipe(cleanCSS()) // Utilisez cleanCSS() à la place de cssmin()
-    .pipe(gulp.dest('dist/css'));
+    .pipe(gulp.dest("dist/css"));
 }
 
 // Fonction pour minifier les fichiers JS
 function minifyJS() {
-  return gulp.src('*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/js'));
+  return gulp.src("*.js").pipe(uglify()).pipe(gulp.dest("dist/js"));
 }
 
 // Tâche par défaut (exécutée en tapant simplement "gulp" dans le terminal)
-gulp.task('default', gulp.series(minifyCSS, minifyJS));
+gulp.task("default", gulp.series(minifyCSS, minifyJS));
